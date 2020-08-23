df1a1d09b1a09325fa4ecbd14a75c3a6d1f9b6c1
amar-rokade <rokade6117@gmail.com>
2020-08-23 16:32:38 +0530

    task algo 2

--- task_algo_2.py
+++ task_algo_2.py
@@ -1,3 +1,8 @@
+
+# ==========================================================
+#           TASK 2 A : Longest Proper Prefix
+# ==========================================================
+
 # Constraints
 # 1 ≤ T ≤ 500
 # 1 ≤ The length of a single expression ≤ 106
@@ -15,7 +20,7 @@
 # 0
 
 
-#input  T
+# #input  T
 T = int(input('Enter the number of expression : '))
 # Initialize input array
 if 1 <= T  and T <=500 :
@@ -23,12 +28,12 @@ if 1 <= T  and T <=500 :
     for i in range(1,T+1):
         exp = list(input('enter expression number {} : '.format(i)))
 
-        if 1 <= len(exp) and len(exp) <= (5 * pow(10,6)):
+        if 1 <= len(exp) and len(exp) <= pow(10,6):
             expression_list.append(exp)
         elif len(exp) == 0:
             print("there's no such a prefix.")
         else :
-            print("expression not valid because it's length more than 10^6!")
+            print("expression not valid because it's length more than  10^6!")
     for expression in expression_list :
         a = 0
         if expression[0] != ">":
@@ -43,3 +48,31 @@ if 1 <= T  and T <=500 :
 else :
     print("Please enter number between 1 and 500..")
 
+
+
+# ==========================================================
+#           TASK 2 B : Max Element in Array (BASIC MODE)
+# ==========================================================
+import array as arr
+initial = []
+n = int(input('Enter number of element in array: '))
+
+if 1 <= n and n <= pow(10,5): 
+    for i in range(n):
+        initial.append(i+1)
+
+    q = int(input('Enter number of queries to be performed: ')) 
+    if 1<= q and q <= 100 :
+        query_list = []
+
+        for i in range(1,q+1):
+            query = arr.array('i',map(int,input('enter expression number {} : '.format(i)).split()))
+            query_list.append(query)
+        for query in query_list:
+            for j in range(query[0]-1,query[1]):
+                initial[j] = initial[j] + query[2]
+        print(max(initial))
+    else :
+        print('Please Enter number of queries between 1 and 100. try again! ')
+else : 
+    print('please  enter number of element less than 10^5!')
