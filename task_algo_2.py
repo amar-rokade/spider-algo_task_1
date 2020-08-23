a08f0aa2326099324ef45e5049ca51a34e698c5e
amar-rokade <rokade6117@gmail.com>
2020-08-23 18:22:58 +0530

    hakker model 1

--- task_algo_2.py
+++ task_algo_2.py
@@ -54,6 +54,7 @@ else :
 #           TASK 2 B : Max Element in Array (BASIC MODE)
 # ==========================================================
 import array as arr
+import sys
 initial = []
 n = int(input('Enter number of element in array: '))
 
@@ -66,8 +67,14 @@ if 1 <= n and n <= pow(10,5):
         query_list = []
 
         for i in range(1,q+1):
-            query = arr.array('i',map(int,input('enter expression number {} : '.format(i)).split()))
-            query_list.append(query)
+            try :
+                query = arr.array('L',map(int,input('enter expression number {} : '.format(i)).split()))
+            except:
+                sys.exit('Please make sure you add correct last element queries! try again')
+            if 1 <= query[0] and query[1]<= n and 1 <= query[2] and query[2] <= pow(10,9):
+                query_list.append(query)
+            else :
+                sys.exit('Please make sure you add correct queries! try again')
         for query in query_list:
             for j in range(query[0]-1,query[1]):
                 initial[j] = initial[j] + query[2]
@@ -76,3 +83,41 @@ if 1 <= n and n <= pow(10,5):
         print('Please Enter number of queries between 1 and 100. try again! ')
 else : 
     print('please  enter number of element less than 10^5!')
+
+
+
+# ==========================================================
+#           TASK 2 B : Max Element in Array (HACKER MODE(1))
+# ==========================================================
+initial = []
+n = int(input('Enter number of element in array: '))
+
+if 1 <= n and n <= pow(10,5): 
+    for i in range(n):
+        initial.append(i+1)
+
+    q = int(float(input('Enter number of queries to be performed: ')))
+    if 1<= q and q <= pow(10,5) :
+        query_list = []
+        for i in range(1,q+1):
+            try :
+                query = arr.array('L',map(int,input('enter expression number {} : '.format(i)).split()))
+            except:
+                sys.exit('Please make sure you add correct last element of query! try again')
+            
+            if 1 <= query[0] and query[1]<= n and 1 <= query[2] and query[2] <= pow(10,9):
+                query_list.append(query)
+            else :
+                sys.exit('Please make sure you add correct queries! try again')
+            
+        for query in query_list:
+            for j in range(query[0]-1,query[1]):
+                initial[j] = initial[j] + query[2]
+        print(max(initial))
+    else :
+        print('Please Enter number of queries between 1 and 10^5. try again! ')
+else : 
+    print('please  enter number of element less than 10^5!')
+
+
+
